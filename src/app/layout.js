fff1533e2da8a26a60829c69715e77f35d73abd5
Roberto <roberto.vukomanovic@gmail.com>
2024-02-01 00:38:57 +0100

    ..

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header/Header";
-import Footer from "@/components/footer/Footer";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
