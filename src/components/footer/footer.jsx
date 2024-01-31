0324da99eda8958ed3cfe9d416b93f1bcb28dcb4
Roberto <roberto.vukomanovic@gmail.com>
2024-02-01 00:30:11 +0100

    export...

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import styles from '@/components/footer/footer.module.scss';
 import { getYear } from 'date-fns'
 
-const Footer = () => {
+export const Footer = () => {
     return (
         <footer className={styles.footerContainer}>
             <p className={styles.copyright}>
