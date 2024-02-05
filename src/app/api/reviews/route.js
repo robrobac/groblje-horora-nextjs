3cae710054848fcb9b2108a3aea738586dbbea0b
Roberto <roberto.vukomanovic@gmail.com>
2024-02-06 00:47:48 +0100

    all reviews route created

---
+++ src/app/api/reviews/route.js
@@ -0,0 +1,165 @@
+
+import { dbConnect } from "@/lib/mongo/dbConnect"
+import reviewModel from "@/lib/mongo/models/reviewModel"
+import { NextResponse } from "next/server"
+
+export const GET = async (request) => {
+    const sort = request.nextUrl.searchParams.get('sort');
+    const order = request.nextUrl.searchParams.get('order');
+    const search = request.nextUrl.searchParams.get('search');
+    const page = request.nextUrl.searchParams.get('page');
+    const perPage = request.nextUrl.searchParams.get('perPage');
+    const filter = request.nextUrl.searchParams.get('filter');
+    console.log(sort, order, search, page, perPage, filter)
+
+    const getOrder = (orderBy) => {
+        if (orderBy === 'desc') {
+            return -1
+        }
+        if (orderBy === 'asc') {
+            return 1
+        }
+    }
+
+    const skip = (page - 1) * perPage
+
+    try {
+        dbConnect()
+
+        if (search) {
+            const reviewsQuery = {
+                $or: [
+                    { $text: { $search: search } },
+                    // { "reviewTitle": { $regex: new RegExp(`.*${search}.*`, 'i') } }
+                ],
+            }
+            //  If SEARCH is in query, do the search.
+            const reviews = await reviewModel.find(reviewsQuery)
+                .skip(skip)
+                .limit(perPage)
+                // .sort([['createdAt', -1]])
+                .sort({ score: { $meta: "textScore" } })  // Sort by relevance
+            const totalReviewsCount = await reviewModel.countDocuments(reviewsQuery)
+
+            return NextResponse.json({
+                reviews,
+                totalItems: totalReviewsCount,
+                totalPages: Math.ceil(totalReviewsCount / perPage)
+            })
+        } else {
+            // Else if there's no Search in the query, continue with sorting and ordering
+
+            if (sort === 'movies.0.rating') {
+                const typeFilter = filter ? { 'reviewType': filter } : {}
+                // sory by rating
+                const reviews = await reviewModel.find(typeFilter)
+                    .skip(skip)
+                    .limit(perPage)
+                    .sort([
+                        ['reviewType', getOrder(order)],
+                        [sort, getOrder(order)],
+                    ]);
+                const totalReviewsCount = await reviewModel.countDocuments(typeFilter)
+
+                return NextResponse.json({
+                    reviews,
+                    totalItems: totalReviewsCount,
+                    totalPages: Math.ceil(totalReviewsCount / perPage)
+                })
+            }
+
+            if (sort === 'reviewTitle') {
+                const typeFilter = filter ? { 'reviewType': filter } : {}
+                // sort by title
+                const reviews = await reviewModel.find(typeFilter)
+                    .skip(skip)
+                    .limit(perPage)
+                    .sort([
+                        ['reviewTitle', getOrder(order)],
+                        [sort, getOrder(order)],
+                    ]);
+                const totalReviewsCount = await reviewModel.countDocuments(typeFilter)
+
+                return NextResponse.json({
+                    reviews,
+                    totalItems: totalReviewsCount,
+                    totalPages: Math.ceil(totalReviewsCount / perPage)
+                })
+            }
+
+            // if (sort === 'reviewType') {
+            //     // sort by type
+            //     const reviews = await reviewModel.find({})
+            //         .skip(skip)
+            //         .limit(perPage)
+            //         .sort([
+            //             ['reviewType', getOrder(order)],
+            //             [sort, getOrder(order)],
+            //         ]);
+            //     const totalReviewsCount = await reviewModel.countDocuments()
+
+            //     return NextResponse.json({
+            //         reviews,
+            //         totalItems: totalReviewsCount,
+            //         totalPages: Math.ceil(totalReviewsCount / perPage)
+            //     })
+            // }
+
+            if (sort === 'createdAt') {
+                const typeFilter = filter ? { 'reviewType': filter } : {}
+                // sort by date created
+                const reviews = await reviewModel.find(typeFilter)
+                    .skip(skip)
+                    .limit(perPage)
+                    .sort([
+                        ['createdAt', getOrder(order)],
+                        [sort, getOrder(order)],
+                    ]);
+                const totalReviewsCount = await reviewModel.countDocuments(typeFilter)
+
+                return NextResponse.json({
+                    reviews,
+                    totalItems: totalReviewsCount,
+                    totalPages: Math.ceil(totalReviewsCount / perPage)
+                })
+            }
+
+            // if (sort === 'updatedAt') {
+            //     // sort by date updated
+            //     const reviews = await reviewModel.find({})
+            //         .skip(skip)
+            //         .limit(perPage)
+            //         .sort([
+            //             ['updatedAt', getOrder(order)],
+            //             [sort, getOrder(order)],
+            //         ]);
+            //     const totalReviewsCount = await reviewModel.countDocuments()
+
+            //     return NextResponse.json({
+            //         reviews,
+            //         totalItems: totalReviewsCount,
+            //         totalPages: Math.ceil(totalReviewsCount / perPage)
+            //     })
+            // }
+
+            if (!sort) {
+                // if there's no sort, fetch by date created
+                const reviews = await reviewModel.find({})
+                    .skip(skip)
+                    .limit(perPage)
+                    .sort({ createdAt: -1 })
+                const totalReviewsCount = await reviewModel.countDocuments()
+
+                return NextResponse.json({
+                    reviews,
+                    totalItems: totalReviewsCount,
+                    totalPages: Math.ceil(totalReviewsCount / perPage)
+                })
+            }
+
+        }
+    } catch (err) {
+        throw new Error('Failed to fetch Reviews', err)
+    }
+}
+
--- src/app/recenzije/page.jsx
+++ src/app/recenzije/page.jsx
@@ -1,11 +1,25 @@
 import styles from "@/app/recenzije/page.module.scss";
 
+export const dynamic = 'force-dynamic';
+
 export const metadata = {
     title: "Recenzije",
-    description: "Recenzije Description",
+    description: "Na ovoj stranici pronađite sve recenzije i kratke preglede od 2007. godine do danas. Koristeći pretraživač filtrirajte, sortirajte, pretražite i pronađite željeni horor film. Ne zamjerite na svim recenzijama za filmove koje su napisane u ranoj fazi bloga, tamo od 2007. do 2010., tada sam bio klinjo od svega 14-15 godina.",
 };
 
-const RecenzijePage = () => {
+const getData = async () => {
+    const res = await fetch(`${process.env.DOMAIN_URL}/api/reviews?page=1&sort=createdAt&order=desc`);
+    console.log("Reviews data fetched")
+    if (!res.ok) {
+        throw new Error('Failed to fetch Reviews data');
+    }
+    return res.json();
+}
+
+const RecenzijePage = async () => {
+    const data = await getData();
+    console.log(data)
+    
     return (
         <main className={styles.reviewsContainer}>
             <div className={styles.reviewsTitleContainer}>
