6d96a9dc49d77fc149b8a5b67d04e235befab7bb
Roberto <roberto.vukomanovic@gmail.com>
2024-02-27 20:26:40 +0100

    test cover image optimisation

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 // next.config.js
 const nextConfig = {
     images: {
-        domains: ['https://firebasestorage.googleapis.com/'], // Replace with your actual domain
-    },
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+                port: '',
+                // pathname: '/account123/**',
+            },
+        ],
+    }
+    // images: {
+    //     domains: ['https://firebasestorage.googleapis.com/'], // Replace with your actual domain
+    // },
     // reactStrictMode: false,
 };
 
