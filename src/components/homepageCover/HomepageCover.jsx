6670c8da1ee63fead03fb466b78d0fe49dd6079d
Roberto <roberto.vukomanovic@gmail.com>
2024-02-28 23:30:18 +0100

    cover image

--- src/components/homepageCover/HomepageCover.jsx
+++ src/components/homepageCover/HomepageCover.jsx
@@ -7,9 +7,7 @@ export const HomepageCover = () => {
 
     return (
         <div className={styles.coverImage}>
-            <Image priority={true} width={2560} height={800} src={'/images/groblje-horora-home-page-cover.webp'} alt='main background, a graveyard silhouette'
-                sizes="(max-width: 425px) 100vw"
-            />
+            <Image priority={true} width={2560} height={800} src={'/images/groblje-horora-home-page-cover.webp'} alt='main background, a graveyard silhouette'/>
             <div className={styles.coverImageTrees}>
                 <Image className={styles.leftTree} priority={true} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the left side of the screen'/>
                 <Image className={styles.rightTree} priority={true} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the right side of the screen'/>
