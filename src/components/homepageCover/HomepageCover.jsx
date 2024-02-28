7c3d3e9d11585d747b6f51277312525355fc3154
Roberto <roberto.vukomanovic@gmail.com>
2024-02-28 14:06:58 +0100

    Revert image optimization

--- src/components/homepageCover/HomepageCover.jsx
+++ src/components/homepageCover/HomepageCover.jsx
@@ -2,15 +2,13 @@
 
 import styles from '@/components/homepageCover/homepageCover.module.scss';
 import Image from 'next/image';
-// import coverImage from '../../../public/images/groblje-horora-home-page-cover.webp'
-import coverImage2 from '../../../public/images/test.jpg'
 
 export const HomepageCover = () => {
 
     return (
         <div className={styles.coverImage}>
-            <Image priority={true} src={coverImage2} alt='main background, a graveyard silhouette'
-                
+            <Image priority={true} width={2560} height={800} src={'/images/groblje-horora-home-page-cover.webp'} alt='main background, a graveyard silhouette'
+                sizes="(max-width: 425px) 100vw"
             />
             <div className={styles.coverImageTrees}>
                 <Image className={styles.leftTree} priority={true} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the left side of the screen'/>
