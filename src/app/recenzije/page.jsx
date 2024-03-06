1c226e1c13561ff38feac73b1d4bbbe96730a97f
Roberto <roberto.vukomanovic@gmail.com>
2024-03-06 18:20:56 +0100

    meta check 2

--- src/app/recenzije/page.jsx
+++ src/app/recenzije/page.jsx
@@ -7,6 +7,15 @@ export const dynamic = 'force-dynamic';
 export const metadata = {
     title: "Recenzije",
     description: "Na ovoj stranici pronađite sve recenzije i kratke preglede od 2007. godine do danas. Koristeći pretraživač filtrirajte, sortirajte, pretražite i pronađite željeni horor film. Ne zamjerite na svim recenzijama za filmove koje su napisane u ranoj fazi bloga, tamo od 2007. do 2010., tada sam bio klinjo od svega 14-15 godina.",
+    openGraph: {
+        title: "Recenzije",
+        description: 'Na ovoj stranici pronađite sve recenzije i kratke preglede od 2007. godine do danas. Koristeći pretraživač filtrirajte, sortirajte, pretražite i pronađite željeni horor film. Ne zamjerite na svim recenzijama za filmove koje su napisane u ranoj fazi bloga, tamo od 2007. do 2010., tada sam bio klinjo od svega 14-15 godina.',
+        url: 'https://www.groblje-horora.com/recenzije',
+        siteName: "Recenzije",
+    },
+    alternates: {
+        canonical: 'https://www.groblje-horora.com/recenzije'
+    }
 };
 
 const RecenzijePage = () => {
