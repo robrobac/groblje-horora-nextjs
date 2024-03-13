1470b0f6d88656c3d79151558a3a05fcda058b36
Roberto <roberto.vukomanovic@gmail.com>
2024-03-13 10:31:28 +0100

    performance test

--- src/components/homepageCover/HomepageCover.jsx
+++ src/components/homepageCover/HomepageCover.jsx
@@ -1,4 +1,4 @@
-'use server'
+'use client'
 
 import styles from '@/components/homepageCover/homepageCover.module.scss';
 import Image from 'next/image';
@@ -7,10 +7,10 @@ export const HomepageCover = () => {
 
     return (
         <div className={styles.coverImage}>
-            <Image priority={true} width={2560} height={800} src={'/images/groblje-horora-home-page-cover.webp'} alt='main background, a graveyard silhouette'/>
+            <Image loading='eager' priority={true} width={2560} height={800} src={'/images/groblje-horora-home-page-cover.webp'} alt='main background, a graveyard silhouette'/>
             <div className={styles.coverImageTrees}>
-                <Image className={styles.leftTree} priority={true} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the left side of the screen'/>
-                <Image className={styles.rightTree} priority={true} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the right side of the screen'/>
+                <Image loading='eager' className={styles.leftTree} priority={true} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the left side of the screen'/>
+                <Image loading='eager' className={styles.rightTree} priority={true} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the right side of the screen'/>
             </div>
         </div>
     )
