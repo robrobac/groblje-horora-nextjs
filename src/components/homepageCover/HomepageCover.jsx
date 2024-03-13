11aa60679aadff662805cf39e8437169b771ba9b
Roberto <roberto.vukomanovic@gmail.com>
2024-03-13 13:29:21 +0100

    one more performance test

--- src/components/homepageCover/HomepageCover.jsx
+++ src/components/homepageCover/HomepageCover.jsx
@@ -2,12 +2,35 @@
 
 import styles from '@/components/homepageCover/homepageCover.module.scss';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export const HomepageCover = () => {
 
+    const [windowWidth, setWindowWidth] = useState(0);
+    console.log(windowWidth)
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        // Initial call to set window width
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className={styles.coverImage}>
-            <Image loading='eager' priority={true} width={2560} height={800} src={'/images/groblje-horora-home-page-cover.webp'} alt='main background, a graveyard silhouette'/>
+            {windowWidth > 768 ? (
+                <Image loading='eager' priority={true} width={2560} height={800} src={'/images/groblje-horora-home-page-cover.webp'} alt='main background, a graveyard silhouette'/>
+            ) : (
+                <Image loading='eager' priority={true} width={2560} height={800} src={'/images/middle-groblje-horora-home-page-cover.webp'} alt='main background, a graveyard silhouette'/>
+            )}
             <div className={styles.coverImageTrees}>
                 <Image loading='eager' className={styles.leftTree} priority={true} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the left side of the screen'/>
                 <Image loading='eager' className={styles.rightTree} priority={true} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the right side of the screen'/>
