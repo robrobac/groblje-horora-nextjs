55576de033aa4b81b23f607fe8296a4f63141494
Roberto <roberto.vukomanovic@gmail.com>
2024-03-15 09:47:02 +0100

    google analytics3

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,7 +36,7 @@ export default function RootLayout({ children }) {
             <head>
                 <Script
                     async
-                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                    src={`https://www.googletagmanager.com/gtag/js?id=G-V2JZKHZKN0`}
                 />
                 <Script id="google-analytics">
                     {`
