2740949b5587c16a282b457be752ba036c1a62ca
Roberto <roberto.vukomanovic@gmail.com>
2024-03-19 17:40:04 +0100

    schema test

--- src/app/page.js
+++ src/app/page.js
@@ -9,8 +9,22 @@ import Worse20 from "@/components/topComponents/Worse20";
 export const dynamic = 'force-dynamic';
 
 export default function Naslovna() {
+
+    const jsonLd = {
+        "@context": "http://schema.org",
+        "@type": "WebPage",
+        image: {
+            "@type": "ImageObject",
+            url: "https://firebasestorage.googleapis.com/v0/b/groblje-horora-89186.appspot.com/o/groblje-horora-og-image.webp?alt=media&token=9505221c-7713-4907-8a95-78047f2cd1b7"
+        }
+    }
+
     return (
         <main>
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+            />
             <HomepageCover />
             <Introduction />
             <LatestPregled />
