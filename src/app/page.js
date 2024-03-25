aedc80079cd45bef4a0b0c8cfce26aa5e5892fa9
Roberto <roberto.vukomanovic@gmail.com>
2024-03-25 11:06:58 +0100

    structured data test

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import { LatestRecenzija } from "@/components/latest/LatestRecenzija";
 import { RecenzijeComponent } from "@/components/recenzijeComponent/RecenzijeComponent";
 import Top25 from "@/components/topComponents/Top25";
 import Worse20 from "@/components/topComponents/Worse20";
+import Script from "next/script";
 
 export const dynamic = 'force-dynamic';
 
@@ -21,7 +22,9 @@ export default function Naslovna() {
 
     return (
         <main>
-            <script
+            <Script
+            id="homeStructuredData"
+            strategy="beforeInteractive"
                 type="application/ld+json"
                 dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
             />
