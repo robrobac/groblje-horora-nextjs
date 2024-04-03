60c717ca836276a8d57890a7b8d720d964671d73
Roberto <roberto.vukomanovic@gmail.com>
2024-04-03 19:36:37 +0200

    sitemap fix 3

--- src/components/header/links/Links.jsx
+++ src/components/header/links/Links.jsx
@@ -5,7 +5,7 @@ export const Links = () => {
     const links = [
         { name: 'Naslovna', path: '/', activeLink: '/' },
         { name: 'Top 25', path: '/top25', activeLink: '/top25' },
-        { name: 'Recenzije', path: '/recenzije?page=1&sort=createdAt&order=desc', activeLink: '/recenzije' },
+        { name: 'Recenzije', path: '/recenzije', activeLink: '/recenzije' },
         { name: 'Top 20 smeća', path: '/top20smeca', activeLink: '/top20smeca' },
         { name: 'O Blogu', path: '/o-blogu', activeLink: '/o-blogu' },
     ];
