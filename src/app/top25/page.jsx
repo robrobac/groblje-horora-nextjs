bb9c0f0fd795900461bc6dd759e8826c99bb8c5e
Roberto <roberto.vukomanovic@gmail.com>
2024-04-10 19:19:21 +0200

    test4

--- src/app/top25/page.jsx
+++ src/app/top25/page.jsx
@@ -9,13 +9,7 @@ export const metadata = {
     openGraph: {
         title: "Top 25 Preporuka",
         description: "Filmovi na ovoj top listi mijenjaju se kako dođe neki novi naslov na blogu koji zaslužuje jednaku pažnju ili ocjenu. Linkove na recenzije možete pronaći na Top 25 popisu.",
-        images: [
-            {
-                url: "/openGraph/top25-open-graph-image.jpg",
-                width: 1200,
-                height: 630,
-            }
-        ],
+        images: "https://firebasestorage.googleapis.com/v0/b/groblje-horora-89186.appspot.com/o/openGraph%2Ftop25-open-graph-image.jpg?alt=media&token=85db0be7-da7d-4f81-bee7-22934ca6909d",
     },
     alternates: {
         canonical: 'https://www.groblje-horora.com/top25'
