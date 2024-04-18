0b106e34776e6d4b8e47dd1c765f4631a7f30aea
Roberto <roberto.vukomanovic@gmail.com>
2024-04-19 01:15:41 +0200

    ga test

--- src/components/googleAnalyticsComp/GoogleAnalyticsComp.jsx
+++ src/components/googleAnalyticsComp/GoogleAnalyticsComp.jsx
@@ -1,17 +1,7 @@
 "use client"
-import { GoogleAnalytics, event } from 'nextjs-google-analytics'
 import React from 'react'
-
-
-export function reportWebVitals({ id, name, label, value }) {
-    event(name, {
-      category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
-      value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
-      label: id, // id unique to current page load
-      nonInteraction: true, // avoids affecting bounce rate.
-    });
-}
-
+import dynamic from "next/dynamic";
+const GoogleAnalytics = dynamic(() => import('nextjs-google-analytics'), { ssr: false })
 
 export default function GoogleAnalyticsComp() {
   return <GoogleAnalytics trackPageViews gaMeasurementId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}/>
