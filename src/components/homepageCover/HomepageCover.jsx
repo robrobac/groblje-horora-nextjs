0b4f337080f03fefef7049b15fd8209a525899c7
Roberto <roberto.vukomanovic@gmail.com>
2024-05-02 01:32:15 +0200

    reverting last commit

--- src/components/homepageCover/HomepageCover.jsx
+++ src/components/homepageCover/HomepageCover.jsx
@@ -2,33 +2,33 @@
 import styles from '@/components/homepageCover/homepageCover.module.scss';
 import Image from 'next/image';
 import cover from '../../../public/images/1920-groblje-horora-home-page-cover.jpg'
-import cover2 from '../../../public/images/500-groblje-horora-home-page-cover.jpg'
+// import cover2 from '../../../public/images/500-groblje-horora-home-page-cover.jpg'
 
 import { Creepster } from "next/font/google";
-import { headers } from 'next/headers';
-import { UAParser } from 'ua-parser-js';
+// import { headers } from 'next/headers';
+// import { UAParser } from 'ua-parser-js';
 
 const creepster = Creepster({ subsets: ["latin"], weight: '400' });
 
 
 
 export const HomepageCover = async () => {
-    const headersList = headers().get('user-agent')
-    let parser = new UAParser(headersList);
-    let parserResults = parser.getResult();
-    console.log(parserResults);
+    // const headersList = headers().get('user-agent')
+    // let parser = new UAParser(headersList);
+    // let parserResults = parser.getResult();
+    // console.log(parserResults);
     return (
         <>
         <div className={styles.coverImage}>
-            <Image priority={true} src={parserResults.device.type == "mobile" ? cover2 : cover} alt='main background, a graveyard silhouette'/>
+            <Image priority={true} src={cover} alt='main background, a graveyard silhouette'/>
             <div className={styles.coverImageTrees}>
-                <Image className={styles.leftTree} priority={true} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the left side of the screen'/>
+                <Image className={styles.leftTree} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the left side of the screen'/>
                 <div className={styles.coverTitleDesktopWrap}>
                     <h1 className={`${creepster.className} ${styles.coverTitleDesktop}`}>Groblje Horora</h1>
                     <h2 className={styles.coverSubtitleDesktop}>Recenzije Horor Filmova i Serija</h2>
                 </div>
                 
-                <Image className={styles.rightTree} priority={true} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the right side of the screen'/>
+                <Image className={styles.rightTree} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the right side of the screen'/>
             </div>
         </div>
             
