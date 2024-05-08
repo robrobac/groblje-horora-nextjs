ac1c636262155ab18db36dc685bcce7fcaae253f
Roberto <roberto.vukomanovic@gmail.com>
2024-05-09 00:42:59 +0200

    removed placeholder from cover image

--- src/components/homepageCover/HomepageCover.jsx
+++ src/components/homepageCover/HomepageCover.jsx
@@ -14,18 +14,32 @@ const creepster = Creepster({ subsets: ["latin"], weight: '400' });
 
 
 export const HomepageCover = async () => {
+    // const headersList = headers().get('user-agent')
+    // let parser = new UAParser(headersList);
+    // let parserResults = parser.getResult();
+    // console.log(parserResults.device.type);
+
+    // const coverImage = parserResults.device.type === "mobile" ? coverMobile : cover;
+
     const headersList = headers().get('user-agent')
     let parser = new UAParser(headersList);
     let parserResults = parser.getResult();
     console.log(parserResults.device.type);
 
-    const coverImage = parserResults.device.type === "mobile" ? coverMobile : cover;
-    
+    // Dynamically import the appropriate cover image based on device type
+    let coverModule;
+    if (parserResults.device.type === "mobile") {
+        coverModule = await import('../../../public/images/425-groblje-horora-home-page-cover.webp');
+    } else {
+        coverModule = await import('../../../public/images/1920-groblje-horora-home-page-cover.webp');
+    }
+    const coverImage = coverModule.default;
+
     return (
         <>
         <div className={styles.coverImage}>
             <Image className={styles.coverImageGraves} src={graves} alt='graves' style={{position: "absolute"}}/>
-            <Image placeholder='blur' priority={true} src={coverImage} alt='main background, a graveyard silhouette'/>
+            <Image priority={true} src={coverImage} alt='main background, a graveyard silhouette'/>
             <div className={styles.coverImageTrees}>
                 <Image className={styles.leftTree} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the left side of the screen'/>
                 <div className={styles.coverTitleDesktopWrap}>
