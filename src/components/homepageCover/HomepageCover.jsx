87fa148abb0b4f722bf2ea7c9834af81049608d0
Roberto <roberto.vukomanovic@gmail.com>
2024-05-09 00:10:04 +0200

    new home page cover image and svg with graves

--- src/components/homepageCover/HomepageCover.jsx
+++ src/components/homepageCover/HomepageCover.jsx
@@ -1,7 +1,8 @@
 
 import styles from '@/components/homepageCover/homepageCover.module.scss';
 import Image from 'next/image';
-import cover from '../../../public/images/1920-groblje-horora-home-page-cover.jpg'
+import cover from '../../../public/images/1920-groblje-horora-home-page-cover.webp'
+import graves from '../../../public/images/groblje-horora-graves.svg'
 // import cover2 from '../../../public/images/500-groblje-horora-home-page-cover.jpg'
 
 import { Creepster } from "next/font/google";
@@ -20,6 +21,7 @@ export const HomepageCover = async () => {
     return (
         <>
         <div className={styles.coverImage}>
+            <Image className={styles.coverImageGraves} src={graves} alt='graves' style={{position: "absolute"}}/>
             <Image priority={true} src={cover} alt='main background, a graveyard silhouette'/>
             <div className={styles.coverImageTrees}>
                 <Image className={styles.leftTree} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the left side of the screen'/>
@@ -27,7 +29,6 @@ export const HomepageCover = async () => {
                     <h1 className={`${creepster.className} ${styles.coverTitleDesktop}`}>Groblje Horora</h1>
                     <h2 className={styles.coverSubtitleDesktop}>Recenzije Horor Filmova i Serija</h2>
                 </div>
-                
                 <Image className={styles.rightTree} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the right side of the screen'/>
             </div>
         </div>
