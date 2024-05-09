db4ffd87b8178145ddc750fdb4139acc49b142b6
Roberto <roberto.vukomanovic@gmail.com>
2024-05-09 21:02:43 +0200

    added cover image for ablet

--- src/components/homepageCover/HomepageCover.jsx
+++ src/components/homepageCover/HomepageCover.jsx
@@ -1,8 +1,6 @@
 
 import styles from '@/components/homepageCover/homepageCover.module.scss';
 import Image from 'next/image';
-import cover from '../../../public/images/1920-groblje-horora-home-page-cover.webp'
-import coverMobile from '../../../public/images/425-groblje-horora-home-page-cover.webp'
 import graves from '../../../public/images/groblje-horora-graves.svg'
 
 import { Creepster } from "next/font/google";
@@ -14,13 +12,6 @@ const creepster = Creepster({ subsets: ["latin"], weight: '400' });
 
 
 export const HomepageCover = async () => {
-    // const headersList = headers().get('user-agent')
-    // let parser = new UAParser(headersList);
-    // let parserResults = parser.getResult();
-    // console.log(parserResults.device.type);
-
-    // const coverImage = parserResults.device.type === "mobile" ? coverMobile : cover;
-
     const headersList = headers().get('user-agent')
     let parser = new UAParser(headersList);
     let parserResults = parser.getResult();
@@ -30,6 +21,8 @@ export const HomepageCover = async () => {
     let coverModule;
     if (parserResults.device.type === "mobile") {
         coverModule = await import('../../../public/images/425-groblje-horora-home-page-cover.webp');
+    } else if (parserResults.device.type === "tablet") {
+        coverModule = await import('../../../public/images/1440-groblje-horora-home-page-cover.webp');
     } else {
         coverModule = await import('../../../public/images/1920-groblje-horora-home-page-cover.webp');
     }
@@ -37,19 +30,18 @@ export const HomepageCover = async () => {
 
     return (
         <>
-        <div className={styles.coverImage}>
-            <Image className={styles.coverImageGraves} src={graves} alt='graves' style={{position: "absolute"}}/>
-            <Image priority={true} src={coverImage} alt='main background, a graveyard silhouette'/>
-            <div className={styles.coverImageTrees}>
-                <Image className={styles.leftTree} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the left side of the screen'/>
-                <div className={styles.coverTitleDesktopWrap}>
-                    <h1 className={`${creepster.className} ${styles.coverTitleDesktop}`}>Groblje Horora</h1>
-                    <h2 className={styles.coverSubtitleDesktop}>Recenzije Horor Filmova i Serija</h2>
+            <div className={styles.coverImage}>
+                <Image className={styles.coverImageGraves} src={graves} alt='graves' style={{position: "absolute"}}/>
+                <Image priority={true} src={coverImage} alt='main background, a graveyard silhouette'/>
+                <div className={styles.coverImageTrees}>
+                    <Image className={styles.leftTree} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the left side of the screen'/>
+                    <div className={styles.coverTitleDesktopWrap}>
+                        <h1 className={`${creepster.className} ${styles.coverTitleDesktop}`}>Groblje Horora</h1>
+                        <h2 className={styles.coverSubtitleDesktop}>Recenzije Horor Filmova i Serija</h2>
+                    </div>
+                    <Image className={styles.rightTree} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the right side of the screen'/>
                 </div>
-                <Image className={styles.rightTree} width={428} height={800} src={'/images/groblje-horora-cover-left-tree.svg'} alt='tree silhouette on the right side of the screen'/>
             </div>
-        </div>
-            
         </>
     )
 }
