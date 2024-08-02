4dd05e300b774ccce7e18d8bf93907598c749d30
Roberto <roberto.vukomanovic@gmail.com>
2024-08-02 10:46:15 +0200

    Navigation top25 active link fix

--- src/components/header/links/Links.jsx
+++ src/components/header/links/Links.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "./NavLink";
 export const Links = () => {
     const links = [
         { name: 'Naslovna', path: `${process.env.NEXT_PUBLIC_DOMAIN_URL}`, activeLink: '/', prefetch: false },
-        { name: 'Top 25', path: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/top25`, activeLink: '', prefetch: false },
+        { name: 'Top 25', path: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/top25`, activeLink: '/top25', prefetch: false },
         { name: 'Recenzije', path: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/recenzije`, activeLink: '/recenzije', prefetch: false },
         { name: 'Top 20 smeća', path: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/top20smeca`, activeLink: '/top20smeca', prefetch: false },
         { name: 'O Blogu', path: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/o-blogu`, activeLink: '/o-blogu', prefetch: false },
