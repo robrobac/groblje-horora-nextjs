bbc4a2cf1a7e9239c75d92a4189eec9de3f87054
Roberto <roberto.vukomanovic@gmail.com>
2025-03-11 13:13:36 +0100

    new nextjs config for image optimisation cache and for now will use unoptimised images

--- next.config.js
+++ next.config.js
@@ -2,28 +2,57 @@
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
-  })
-
-const nextConfig = {
+  });
+  
+  const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-                port: '',
-                // pathname: '/account123/**',
-            },
-        ],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'firebasestorage.googleapis.com',
+          port: '',
+          // pathname: '/account123/**',
+        },
+      ],
+    //   minimumCacheTTL: 2678400, // Cache images for 31 days
+      minimumCacheTTL: 31536000, // Cache images for 1 year
+      unoptimized: true, // Temporarily disable image optimization
     },
     logging: {
-        fetches: {
-            fullUrl: true,
-        },
+      fetches: {
+        fullUrl: true,
+      },
     },
-    // images: {
-    //     domains: ['https://firebasestorage.googleapis.com/'], // Replace with your actual domain
-    // },
-    // reactStrictMode: false,
-};
+    reactStrictMode: false,
+  };
+  
+  module.exports = withBundleAnalyzer(nextConfig);
 
-module.exports = withBundleAnalyzer(nextConfig)
+
+  // const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//     enabled: process.env.ANALYZE === 'true',
+//   })
+
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 'firebasestorage.googleapis.com',
+//                 port: '',
+//                 // pathname: '/account123/**',
+//             },
+//         ],
+//     },
+//     logging: {
+//         fetches: {
+//             fullUrl: true,
+//         },
+//     },
+//     // images: {
+//     //     domains: ['https://firebasestorage.googleapis.com/'], // Replace with your actual domain
+//     // },
+//     // reactStrictMode: false,
+// };
+
+// module.exports = withBundleAnalyzer(nextConfig)
